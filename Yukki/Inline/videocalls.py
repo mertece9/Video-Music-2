f76b9533130f8ab8d0e3896bdd504d52b86e612d
HariboTube <57761676+HariboTube@users.noreply.github.com>
2022-08-16 15:38:34 +0300

    Update videocalls.py

--- Yukki/Inline/videocalls.py
+++ Yukki/Inline/videocalls.py
@@ -6,17 +6,17 @@ def choose_markup(videoid, duration, user_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🎵 Oynat",
+                text="🎵 ᴅɪɴʟᴇ",
                 callback_data=f"MusicStream {videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="🎥 izle",
+                text="🎥 ɪᴢʟᴇ",
                 callback_data=f"Choose {videoid}|{duration}|{user_id}",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="❌ Çıkış",
+                text="< ᴋᴀᴘᴀᴛ >",
                 callback_data=f"forceclose {videoid}|{user_id}",
             )
         ],
@@ -28,11 +28,11 @@ def livestream_markup(quality, videoid, duration, user_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🎥 Yayına Başla",
+                text="🎥 ʏᴀʏɪɴ ᴀᴄ",
                 callback_data=f"LiveStream {quality}|{videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="❌ Kapat",
+                text="< ᴋᴀᴘᴀᴛ >",
                 callback_data=f"forceclose {videoid}|{user_id}",
             ),
         ],
@@ -44,25 +44,25 @@ def stream_quality_markup(videoid, duration, user_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="📽 360P",
+                text="📽 360ᴘ",
                 callback_data=f"VideoStream 360|{videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="📽 720P",
+                text="📽 720ᴘ",
                 callback_data=f"VideoStream 720|{videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="📽 480P",
+                text="📽 480ᴘ",
                 callback_data=f"VideoStream 480|{videoid}|{duration}|{user_id}",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="🔙 Geri",
+                text="• ɢᴇʀɪ",
                 callback_data=f"gback_list_chose_stream {videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="❌ Çıkış",
+                text="< ᴄɪᴋɪs >",
                 callback_data=f"forceclose {videoid}|{user_id}",
             ),
         ],
